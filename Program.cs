ed6b72d99c792e62701f6a73b48480c041aaecec
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-12-01 14:16:39 -0500

    Version 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        return 0;
+    }
+
+
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        Console.WriteLine("2 -> " + s.SearchInsert([1, 3, 5, 6], 5));
+        Console.WriteLine("1 -> " + s.SearchInsert([1, 3, 5, 6], 2));
+        Console.WriteLine("7 -> " + s.SearchInsert([1, 3, 5, 6], 7));
+
+    }
+
+}
