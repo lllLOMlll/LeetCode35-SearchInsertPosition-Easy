a144a33d933399128e2862fc084c5810178cca37
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-12-01 14:44:15 -0500

    First test

--- Program.cs
+++ Program.cs
@@ -2,9 +2,26 @@
 {
     public int SearchInsert(int[] nums, int target)
     {
-        
-        // Version 30
-        return 0;
+        if (nums.Length == 0) return 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                return i;
+            }
+
+            if (nums[i] > target)
+            {
+                return i - 1;
+            }
+
+            if (i == nums.Length - 1 && nums[i] < target)
+            {
+                return i + 1;
+            }
+        }
+        return -1;
     }
 
 
@@ -13,7 +30,7 @@
         Solution s = new Solution();
         Console.WriteLine("2 -> " + s.SearchInsert([1, 3, 5, 6], 5));
         Console.WriteLine("1 -> " + s.SearchInsert([1, 3, 5, 6], 2));
-        Console.WriteLine("7 -> " + s.SearchInsert([1, 3, 5, 6], 7));
+        Console.WriteLine("4 -> " + s.SearchInsert([1, 3, 5, 6], 7));
 
     }
 
